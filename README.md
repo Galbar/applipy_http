2276a1b5e3c37f160e7790e4af1c985e9ba34821
Alessio Linares <gitlab@alessio.cc>
2020-05-06 19:25:47 +0200

    Update dependency specification

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Applipy Web
 
-    pip install git+ssh://git@gitlab.com/Galbar2/applipy.git#egg=applipy_web&subdirectory=applipy_web
+    pip install applipy_web@git+ssh://git@gitlab.com/Galbar2/applipy.git@<VERSION>#subdirectory=applipy_web
--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(
     packages=find_packages(exclude=['doc', 'tests']),
     data_files=[],
     python_requires='~=3.6',
-    dependency_links=['git+ssh://git@gitlab.com/Galbar2/applipy.git#egg=applipy&subdirectory=applipy',
-                      'git+ssh://git@gitlab.com/Galbar2/applipy.git#egg=applipy_metrics&subdirectory=applipy_metrics'],
-    install_requires=['aiohttp==3.2.1',
+    install_requires=[f'applipy@git+ssh://git@gitlab.com/Galbar2/applipy.git@{Version.RELEASE}#&subdirectory=applipy',
+                      f'applipy_metrics@git+ssh://git@gitlab.com/Galbar2/applipy.git@{Version.RELEASE}#&subdirectory=applipy_metrics',
+                      'aiohttp==3.2.1',
                       'aiohttp_cors==0.7.0'],
     scripts=[],
 )
