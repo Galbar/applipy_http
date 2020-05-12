d70a9fbbe7c88c9ceb8b1cf0ef58ef02672b0ad9
Alessio Linares <gitlab@alessio.cc>
2020-05-12 16:32:47 +0200

    Fix import bug and bump version

--- applipy_web/module.py
+++ applipy_web/module.py
@@ -2,8 +2,8 @@ import logging
 
 from aiohttp import web
 
-from applipy import Config, LoggingModule, MetricsModule, Module, with_names
-
+from applipy import Config, LoggingModule, MetricsModule, Module
+from applipy_inject import with_names
 from applipy_web.handle import WebConfig, WebHandle
 
 
